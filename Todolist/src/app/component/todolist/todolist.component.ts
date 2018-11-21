6f28c20b3fe77a2bd7abd4ba7e2353360528662f
unknown <patelgaurav1202@gmail.com>
2018-11-21 21:38:39 +0530

    TOdolist

--- Todolist/src/app/component/todolist/todolist.component.ts
+++ Todolist/src/app/component/todolist/todolist.component.ts
@@ -16,32 +16,7 @@ export class TodolistComponent implements OnInit {
   constructor(public TestService:TestService, public http:HttpClient) { }
 
   ngOnInit() {
-  	this.getlist()
-  	this.test()
-    this.getlistData()
+ 
   }
-    test(){
-    let character = this.TestService.getLocation1();
-    let characterHomeworld = this.TestService.getLocation2();
-
-    forkJoin([character, characterHomeworld]).subscribe(results => {
-      console.log(results[0],results[1])
-    });
-   }
-  getlist(){
-	this.TestService.getLocation().subscribe(data => {
-	this.dataObject=data
-	console.log(this.dataObject)
- })
-};
-getlistData(){
-     this.TestService.getdata().subscribe(data => {
-
-          this.table=data.sort(function(a , b){
-            let textA = a.name.toUpperCase();
-             let textB = b.name.toUpperCase();
-          return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-    
-   });
-}}
+   
 }
